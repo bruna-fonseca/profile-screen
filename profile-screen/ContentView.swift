b382da4947034e477fc23165786d92700c7fd82c
Bruna Fonseca <bruna.mfonseca@mercadolivre.com>
2023-04-16 15:03:31 -0300

    Initial Commit

---
+++ profile-screen/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  profile-screen
+//
+//  Created by Bruna Marques De Oliveira Fonseca on 16/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ profile-screen/profile_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_screenApp.swift
+//  profile-screen
+//
+//  Created by Bruna Marques De Oliveira Fonseca on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct profile_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
